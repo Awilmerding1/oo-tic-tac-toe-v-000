bffcf1b9d7482591b379e6943cbe8bccc683db3c
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-02 21:18:08 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,6 +21,16 @@ class TicTacToe
   @board[index] = token
   return @board[index]
 end
-
+  def position_taken?(board, index)
+  if @board[index] == " "
+    return false
+    elsif @board[index] == ""
+    return false
+    elsif @board[index] == nil 
+    return false
+    elsif @board[index] == "X" || @board[index] == "O"
+    return true
+end
+end
 
 end
